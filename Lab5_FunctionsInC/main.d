7a1bd9b3572fc2acade65ba94fb4f1ef84b01f82
Josh Illes <joshua.t.illes@gmail.com>
2017-05-07 09:17:38 -0400

    .gitignore is now working again?

--- Lab5_FunctionsInC/main.d
+++
@@ -1,4 +0,0 @@
-.\main.o: main.c
-.\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-.\main.o: uart.h
-.\main.o: TExaS.h
--- Lab5_FunctionsInC/uart.d
+++
@@ -1,3 +0,0 @@
-.\uart.o: UART.c
-.\uart.o: UART.h
-.\uart.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
